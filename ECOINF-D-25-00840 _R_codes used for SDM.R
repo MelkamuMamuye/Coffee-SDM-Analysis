0d53e009c329d869b1d8d51510613b5c310df3cc
MelkamuMamuye <114362248+MelkamuMamuye@users.noreply.github.com>
2025-08-08 14:41:08 +0300

    ECOINF-D-25-00840 _R_codes used for SDM.R

--- ECOINF-D-25-00840 _R_codes used for SDM.R
+++ ECOINF-D-25-00840 _R_codes used for SDM.R
@@ -1,6 +1,5 @@
-#The r script follows the following steps to perform species distribution 
-#modelling for Coffee arabica#
-#ACTIVATE LIBRARIES
+#The R script follows the following steps to perform species distribution modelling for Coffee arabica
+#Install Packages/ACTIVATE LIBRARIES
 library(sdm)
 library(shiny)
 library(sp)
@@ -17,17 +16,18 @@ library(usdm)
 library(ggspatial)
 library(patchwork)
 library(parallel)
+
 #clear R environment and plots
 rm(list=ls()) # clear the global environment
 dev.off()
 
 # Environmental predictor variables
-#  create a a folder containing study area boundary shape file and load study area polygon 
+#  create a folder containing the study area boundary shape file and load the study area polygon from the drive 
 site = read_sf('C:/PACSMAC/Objective 2 data/study_districts/Zones4.shp')
 site1 = read_sf('C:/PACSMAC/Objective 2 data/study_districts/Zones4.shp') %>% filter (W_NAME=='Limu Seka')
 
-# load predictor variables 
-#### load soil
+### load predictor variables from the drive to access data obtained from diffrent sources
+## load soil data obtained from: https://soilgrids.org/
 setwd('C:/PACSMAC/Objective 2 data/Soil_data/Averaged_soil_data/zones4')
 
 file = list.files(pattern='.tif')
@@ -37,8 +37,8 @@ soil = mask(crop(soil,site),site)
 soil[[3]]=soil[[3]]/10
 plot(soil)
 
-# load topographical variables
-
+### load topographical variables from a digital elevation model (DEM) derived from the Shuttle Radar Topography Mission’s (SRTM),
+### downloaded from http://e0srp01u.ecs.nasa.gov/srtm/version2/SRTM3/
 setwd('C:/PACSMAC/Objective 2 data/Terrain_data/Derived_terrain_variables/zones4')
 file1 = list.files(pattern='.tif')
 topo= raster::stack(file1)
@@ -46,7 +46,7 @@ names(topo) = c('aspect','Elvation','slope')
 topo = mask(crop(topo,site),site)
 names(topo)
 
-# load historical GCM : time range  1970-2000
+### load historical GCM : time range  1970-2000 obtained from WorldClim version 2.1 : https://www.worldclim.org/data
 setwd('D:/Historical_GCM')
 file2 = list.files(pattern='.tif')
 climate = raster::stack(file2)
@@ -57,15 +57,15 @@ names(climate) = c('bio1','bio2','bio3','bio4','bio5','bio6','bio7','bio8','bio9
                    'bio11','bio12','bio13','bio14','bio15','bio16','bio17','bio18','bio19')
 names(climate)
 
-# mask the the historical GCM .tiff file to the extent of study areas
+## mask the the historical GCM .tiff file to the extent of study areas
 climate = mask(crop(climate,site),site)
 names(climate)
 
-# combine all predictor variables
+## combine all predictor variables
 pred = raster::stack(climate,topo,soil)
 names(pred)
 
-#------------spatial thinning of species occurrence/prescence data
+##------spatial thinning of species occurrence/prescence data
 pp = read.csv('C:/PACSMAC/Objective 2 data/Species_prescence_data/Original_datasets/Final data sets/Limu_geopointsFinal.csv')
 pp = pp %>% filter(district=='Limu seka') %>% dplyr::select(X,Y,Species)
 pp$X = as.numeric(pp$X)
@@ -83,76 +83,78 @@ thinned_df = thin(loc.data = pp,
                   write.log.file = FALSE)
 
 th = thinned_df[[1]]
+
 ### partition the the cleaned occurrence data set for training and testing the model
-# partition presence data for train and test the model
+### partition presence data for train and test the model
 set.seed(123)
 partition = sample(nrow(th),0.7*nrow(th))
 presTrain = th[partition,]
 presTest = th[-partition,]
 
-#---------------------------- adding PA column to specify species present
+###---------------------------- adding PA column to specify species present
 presTrain$PA = 1
 presTest$PA = 1
 
-# create spatial dataframe
+### create spatial dataframe
 
 coordinates(presTrain) = ~ Longitude + Latitude
 coordinates(presTest) = ~ Longitude + Latitude
 
-# check multi-collinearity of CGM data
+### check multi-collinearity of CGM data
 pred_df = raster::extract(pred,th) %>% data.frame()
 var = usdm::vifstep(pred_df)
 
-# retain variables without multi-collinearity problems 
+### retain variables without multi-collinearity problems 
 pred = exclude(pred, var)
 names(pred)
 
-#--------------create sdm data object-------------------##
+###-------------create sdm data object-------------------##
 d = sdmData(PA~.,train=presTrain,predictors = pred,bg=list(n=1000))
 
-#---------------model fitting------------------###
+###---------------model fitting------------------###
 m = sdm(PA~., d, methods =c('maxent','rf', 'svm'),replication='sub',test.percent=30,n=5)
 
-# visualize model performance using shiny app
+### visualize model performance using shiny app
 gui(m) 
 
-# ROC-AUC (AREA UNDER CURVE plots)
+### ROC-AUC (AREA UNDER CURVE plots)
 roc(m,smooth=T)
 
-# variable importance values
+### variable importance values
 varI = getVarImp(m)
 
-# get data frame for variable importance
+### get data frame for variable importance
 VariabIm = data.frame(variable=varI@varImportanceMean[["AUCtest"]][["variables"]],
                       AUCmean= varI@varImportanceMean[["AUCtest"]][["AUCtest"]],
                       AUClower=varI@varImportanceMean[["AUCtest"]][["lower"]],
                       AUCupper=varI@varImportanceMean[["AUCtest"]][["upper"]])
 
-# plot variable importance
+### plot variable importance
 p1 = ggplot(VariabIm,aes(x=reorder(variable,AUCmean),y=AUCmean))+geom_col(fill='blue4')+
   geom_errorbar(aes(ymin=AUClower,ymax=AUCupper))+
   coord_flip()+labs(x='Variables',y='Relative variable importance')+
   theme_minimal()+scale_y_continuous(expand = c(0,0.01),labels =scales::percent)+
   theme(axis.ticks.x = element_blank())+ ggtitle('Limu Seka')
 p1
-###save the VI(variable importance) plot to the drive
+
+### save the VI(variable importance) plot to the drive
 ggsave('D:\\Ensemble_output2\\variable_importance.png',p1,height=4,width=7,units='in')
 
-#save the Data frame of VI to the drive in .csv file
+### save the Data frame of VI to the drive in .csv file
 write.csv(VariabIm,'C:\\PACSMAC\\Objective 2 data\\Individual GCM projections\\Variable_importance\\Limu_arabica_VI.csv')
 
-# spatial prediction
+### spatial prediction
 p1 = predict(m,pred)
 p1 = mask(crop(p1,site1),site1)
 
-# Model Ensemble predictions
+### Model Ensemble predictions
 en1 = ensemble(m,p1,setting=list(method='weighted',stat='TSS'))
 
-#----------evaluate model ensemble performance--------###
+### ----------evaluate model ensemble performance--------###
 ev1 = getEvaluation(m,stat=c('AUC','TSS','threshold'),opt = 2)
 ensemble_thd = mean(ev1$threshold)
 
-#-------------create binary raster for ensemble-------###
+###-------------create binary raster for ensemble-------###
 site2 = site1 %>% group_by(W_NAME)%>%summarize()
 par(mfrow=c(1,2))
 
@@ -165,20 +167,20 @@ plot(site2[0],add=T)
 points(th,pch=20,cex=0.6,col='darkred')
 current = en1>=ensemble_thd 
 
-# save the current suitability raster to the drive 
+### save the current suitability raster to the drive 
 writeRaster(current,'C:\\PACSMAC\\Objective 2 data\\Individual GCM projections\\Current_proj\\Limu_current_arabica_projection.tif',overwrite=T)
 
-# group current projections as suitable and non suitable class#
-current1 = as.data.frame(as.data.frame(current,xy=T,period='current',GCM='current')%>%drop_na())
+### group current projections as suitable and non suitable class#
 
+current1 = as.data.frame(as.data.frame(current,xy=T,period='current',GCM='current')%>%drop_na())
 current1 = current1 %>% mutate(binary=ifelse(ensemble_weighted== TRUE,'Suitable','Unsuitable'))
 
-#---------- response curves of retained bioclimatic variables
+###---------- response curves of retained bioclimatic variables
 sdm::rcurve(m)
 
-#-------------------------------------------------------------------------------
-# process future climate scenarios and other biophysical data
-# retain all needed GCMs obtained from worldclim database (https://www.worldclim.org/data/worldclim21.html)
+##########--------------------------------------------------------------
+#### process future climate scenarios and other biophysical data
+#### retain all needed GCMs obtained from worldclim database version 2.1  (https://www.worldclim.org/data/worldclim21.html)
 x = c('wc2.1_30s_bioc_INM-CM5-0_','wc2.1_30s_bioc_MIROC6_','wc2.1_30s_bioc_IPSL-CM6A-LR_',
       'wc2.1_30s_bioc_HadGEM3-GC31-LL_','wc2.1_30s_bioc_UKESM1-0-LL_','wc2.1_30s_bioc_MRI-ESM2-0_',
       'wc2.1_30s_bioc_ACCESS-CM2_','wc2.1_30s_bioc_MPI-ESM1-2-HR_','wc2.1_30s_bioc_CMCC-ESM2_')
@@ -227,7 +229,7 @@ for(i in 1:length(x)){
   }
 }
 
-# load stored binary projection maps and convert to dataframe
+### load stored binary projection maps and convert to dataframe
 dat1 = data.frame()
 
 for(i in 1:length(x)){
@@ -250,7 +252,7 @@ dat1$period[dat1$period=='ssp585_2041-2060.tif']='2041-2060'
 dat1$period[dat1$period=='ssp585_2061-2080.tif']='2061-2080'
 dat1$period[dat1$period=='ssp585_2081-2100.tif']='2081-2100'
 
-# make GCM ensemble predictions from suitability index maps
+### make GCM ensemble predictions from suitability index maps
 z = c('2021-2040','2041-2060','2061-2080','2081-2100')
 mean = data.frame()
 for(h in 1:length(z)){
@@ -381,7 +383,7 @@ mat7$model = factor(mat7$model,levels=c('Suitable','Unsuitable'))
 mat8 = caret::confusionMatrix(mat7$model,mat7$reference)
 mat8
 
-#------------------------------- NICHE CHANGE-----------------------------------
+###------------------------------ NICHE CHANGE-----------------------------------
 Future_proj1 = mat1 %>% mutate(category=ifelse(reference=='Suitable' & model=='Suitable',
                                                'Stable',ifelse(reference=='Suitable' & model=='Unsuitable',
                                                                'Loss',ifelse(reference=='Unsuitable' & model=='Suitable',
@@ -401,11 +403,11 @@ Future_proj4 = mat7 %>% mutate(category=ifelse(reference=='Suitable' & model=='S
                                                                'Loss',ifelse(reference=='Unsuitable' & model=='Suitable',
                                                                              'Gain','none'))),period='2081-2100')
 
-# merge the dataframe
+#33 merge the dataframe
 future_df = rbind(Future_proj1,Future_proj2,Future_proj3)
 future_df = rbind(future_df,Future_proj4)
 
-# plot niche change
+### plot niche change
 p5 = ggplot()+geom_raster(data=filter(future_df,category!='none'),aes(x=x,y=y,fill=category))+
   geom_sf(data=site1,fill='transparent')+
   labs(x='',y='',fill='Change in suitability area')+
@@ -414,23 +416,25 @@ p5 = ggplot()+geom_raster(data=filter(future_df,category!='none'),aes(x=x,y=y,fi
   annotation_scale(location='br')+
   facet_wrap(~period)+theme_bw()+scale_x_continuous(breaks = seq(34.7,353.0,0.3))+
   theme(legend.position = 'bottom')
-# SAVING THE MAP to your drive
+
+### SAVING THE MAP to your drive
 setwd('D:/Ensemble_output2/Limu_ssp585')
 ggsave('LimuB_Arabica niche change ssp585.png',p5,height=9,width=10,units='in')
 #---------------------------------THE END---------------------------------------
 
-## R codes used for the EUDR_comparison
-#LIBRARIES
+### R codes used for the EUDR_comparison
+#install packages/activate libraries LIBRARIES
 library(tidyverse)
 library(terra)
 
 #WORKING DIRECTORY
-setwd("C:/PACSMAC/Objective 2 data/Ethiopia EUDR Comparisons") ##(drop here in individual folders)/Ethiopia EUDR Comparisons
-#GET FILES
+
+setwd("C:/PACSMAC/Objective 2 data/Ethiopia EUDR Comparisons") ##(drop here in individual folders of projeted suitability raster maps)/Ethiopia EUDR Comparisons
 tifs <- list.files(pattern="new_area", recursive=TRUE)
 
-eudr_1 <- rast("JRC_GFC2020_V1_N10_E30.tif")
-eudr_2 <- rast("JRC_GFC2020_V1_N20_E30.tif")
+### this file is obtained from: https://forobs.jrc.ec.europa.eu/GFC#data_download##
+eudr_1 <- rast("JRC_GFC2020_V1_N10_E30.tif")   
+eudr_2 <- rast("JRC_GFC2020_V1_N20_E30.tif")   
 
 eudr <- merge(eudr_1, eudr_2)
 
@@ -474,5 +478,6 @@ g <- ggplot(data=out, aes(x = year, y = prop_eudr*100, fill=ssp, shape = ssp)) +
 
 png("EUDR_forest.png", width = 8, height = 6, units = "in", res=150)
 print(g)
+
 
 ##========================= The end==================#
